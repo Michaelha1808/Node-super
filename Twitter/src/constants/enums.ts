28628f00baad32e1ff66781779920a9479f8f8cd
Michaelha1808 <99546765+Michaelha1808@users.noreply.github.com>
2023-12-20 10:39:06 +0700

    Upload multiple image

--- Twitter/src/constants/enums.ts
+++ Twitter/src/constants/enums.ts
@@ -9,3 +9,8 @@ export enum TokenType {
   ForgotPasswordToken,
   EmailVerifyToken
 }
+
+export enum MediaType {
+  Image,
+  Video
+}
--- Twitter/src/services/medias.services.ts
+++ Twitter/src/services/medias.services.ts
@@ -1,23 +1,33 @@
 import { Request } from 'express'
-import { getNameFromFullName, handleUploadSingleImage } from '~/utils/file'
+import { getNameFromFullName, handleUploadImage } from '~/utils/file'
 import sharp from 'sharp'
 import { UPLOAD_DIR } from '~/constants/dir'
 import path from 'path'
 import fs from 'fs'
 import { isProduction } from '~/constants/config'
 import { config } from 'dotenv'
+import { MediaType } from '~/constants/enums'
+import { Media } from '~/models/Other'
 config()
 
 class MediasService {
-  async handleUploadSingleImage(req: Request) {
-    const file = await handleUploadSingleImage(req)
-    const newName = getNameFromFullName(file.newFilename)
-    const newPath = path.resolve(UPLOAD_DIR, `${newName}.jpg`)
-    await sharp(file.filepath).jpeg().toFile(newPath)
-    fs.unlinkSync(file.filepath)
-    return isProduction
-      ? `${process.env.HOST}/static/${newName}.jpg`
-      : `http://localhost:${process.env.PORT}/static/${newName}.jpg`
+  async uploadImage(req: Request) {
+    const files = await handleUploadImage(req)
+    const result: Media[] = await Promise.all(
+      files.map(async (file) => {
+        const newName = getNameFromFullName(file.newFilename)
+        const newPath = path.resolve(UPLOAD_DIR, `${newName}.jpg`)
+        await sharp(file.filepath).jpeg().toFile(newPath)
+        fs.unlinkSync(file.filepath)
+        return {
+          url: isProduction
+            ? `${process.env.HOST}/static/${newName}.jpg`
+            : `http://localhost:${process.env.PORT}/static/${newName}.jpg`,
+          type: MediaType.Image
+        }
+      })
+    )
+    return result
   }
 }
 
