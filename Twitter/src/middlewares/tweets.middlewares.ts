7fdd8715007a34be835c357ef56966fecd0c0b97
Michaelha1808 <haminhchi1808@gmail.com>
2024-01-02 16:54:59 +0700

    Code logic createTweetValidator section 2

--- Twitter/src/middlewares/tweets.middlewares.ts
+++ Twitter/src/middlewares/tweets.middlewares.ts
@@ -7,19 +7,19 @@ import { numberEnumToArray } from '~/utils/commons'
 import { validate } from '~/utils/validation'
 
 const tweetTypes = numberEnumToArray(TweetType)
-const tweetAudience = numberEnumToArray(TweetAudience)
-const mediaType = numberEnumToArray(MediaType)
+const tweetAudiences = numberEnumToArray(TweetAudience)
+const mediaTypes = numberEnumToArray(MediaType)
 export const createTweetValidator = validate(
   checkSchema({
     type: {
       isIn: {
-        options: tweetTypes,
+        options: [tweetTypes],
         errorMessage: TWEETS_MESSAGES.INVALID_TYPE
       }
     },
     audience: {
       isIn: {
-        options: tweetAudience,
+        options: [tweetAudiences],
         errorMessage: TWEETS_MESSAGES.INVALID_AUDIENCE
       }
     },
@@ -27,14 +27,15 @@ export const createTweetValidator = validate(
       custom: {
         options: (value, { req }) => {
           const type = req.body.type as TweetType
-          // parent id must be tweet_id of tweet paren
+          // Nếu `type` là retweet, comment, quotetweet thì `parent_id` phải là `tweet_id` của tweet cha
           if ([TweetType.Retweet, TweetType.Comment, TweetType.QuoteTweet].includes(type) && !ObjectId.isValid(value)) {
             throw new Error(TWEETS_MESSAGES.PARENT_ID_MUST_BE_A_VALID_TWEET_ID)
           }
-          // parent id is null
-          if (type == TweetType.Tweet && value !== null) {
+          // nếu `type` là tweet thì `parent_id` phải là `null`
+          if (type === TweetType.Tweet && value !== null) {
             throw new Error(TWEETS_MESSAGES.PARENT_ID_MUST_BE_NULL)
           }
+          return true
         }
       }
     },
@@ -43,22 +44,22 @@ export const createTweetValidator = validate(
       custom: {
         options: (value, { req }) => {
           const type = req.body.type as TweetType
-          const hastags = req.body.hastags as string[]
+          const hashtags = req.body.hashtags as string[]
           const mentions = req.body.mentions as string[]
-
-          // parent id must be tweet_id of tweet paren
+          // Nếu `type` là comment, quotetweet, tweet và không có `mentions` và `hashtags` thì `content` phải là string và không được rỗng
           if (
             [TweetType.Comment, TweetType.QuoteTweet, TweetType.Tweet].includes(type) &&
-            isEmpty(hastags) &&
+            isEmpty(hashtags) &&
             isEmpty(mentions) &&
-            value == ''
+            value === ''
           ) {
+            throw new Error(TWEETS_MESSAGES.CONTENT_MUST_BE_A_NON_EMPTY_STRING)
+          }
+          // Nếu `type` là retweet thì `content` phải là `''`.
+          if (type === TweetType.Retweet && value !== '') {
             throw new Error(TWEETS_MESSAGES.CONTENT_MUST_BE_EMPTY_STRING)
           }
-          // parent id is null
-          if (type == TweetType.Retweet && value !== '') {
-            throw new Error(TWEETS_MESSAGES.CONTENT_MUST_BE_EMPTY_STRING)
-          }
+          return true
         }
       }
     },
@@ -66,10 +67,11 @@ export const createTweetValidator = validate(
       isArray: true,
       custom: {
         options: (value, { req }) => {
-          // all elements in array must be strings
-          if (!value.every((item: any) => typeof item === 'string')) {
+          // Yêu cầu mỗi phần từ trong array là string
+          if (value.some((item: any) => typeof item !== 'string')) {
             throw new Error(TWEETS_MESSAGES.HASHTAGS_MUST_BE_AN_ARRAY_OF_STRING)
           }
+          return true
         }
       }
     },
@@ -77,10 +79,11 @@ export const createTweetValidator = validate(
       isArray: true,
       custom: {
         options: (value, { req }) => {
-          // all elements in array must be user_id
-          if (!value.every((item: any) => ObjectId.isValid(item))) {
+          // Yêu cầu mỗi phần từ trong array là user_id
+          if (value.some((item: any) => !ObjectId.isValid(item))) {
             throw new Error(TWEETS_MESSAGES.MENTIONS_MUST_BE_AN_ARRAY_OF_USER_ID)
           }
+          return true
         }
       }
     },
@@ -88,14 +91,15 @@ export const createTweetValidator = validate(
       isArray: true,
       custom: {
         options: (value, { req }) => {
-          // all elements in array must be Media object
+          // Yêu cầu mỗi phần từ trong array là Media Object
           if (
-            !value.some((item: any) => {
-              return typeof item.url !== 'string' || !mediaType.includes(item.type)
+            value.some((item: any) => {
+              return typeof item.url !== 'string' || !mediaTypes.includes(item.type)
             })
           ) {
-            throw new Error(TWEETS_MESSAGES.MENTIONS_MUST_BE_AN_ARRAY_OF_USER_ID)
+            throw new Error(TWEETS_MESSAGES.MEDIAS_MUST_BE_AN_ARRAY_OF_MEDIA_OBJECT)
           }
+          return true
         }
       }
     }
