7c6aa44db2cd89b2eddd689b85e122414bf8ddf7
Michaelha1808 <haminhchi1808@gmail.com>
2024-02-22 14:02:17 +0700

    change search route

--- Twitter/src/routes/search.routes.ts
+++ Twitter/src/routes/search.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
 import { searchController } from '~/controllers/search.controllers'
 import { searchValidator } from '~/middlewares/search.middlewares'
+import { paginationValidator } from '~/middlewares/tweets.middlewares'
 import { accessTokenValidator, verifiedUserValidator } from '~/middlewares/users.middlewares'
 const searchRouter = Router()
 
-searchRouter.get('/', accessTokenValidator, verifiedUserValidator, searchValidator, searchController)
+searchRouter.get(
+  '/',
+  accessTokenValidator,
+  verifiedUserValidator,
+  searchValidator,
+  paginationValidator,
+  searchController
+)
 
 export default searchRouter
