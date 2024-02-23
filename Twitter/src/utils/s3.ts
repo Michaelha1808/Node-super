02dad9cd11be5ebf5fb37357a7fcd30cc6129a88
Michaelha1808 <haminhchi1808@gmail.com>
2024-02-23 17:32:43 +0700

    Upload file into S3

--- Twitter/src/utils/s3.ts
+++ Twitter/src/utils/s3.ts
@@ -1,5 +1,9 @@
 import { S3 } from '@aws-sdk/client-s3'
+import { Upload } from "@aws-sdk/lib-storage";
+import fs from 'fs'
+
 import { config } from 'dotenv'
+import path from 'path';
 config()
 const s3 = new S3({
   region: process.env.AWS_REGION,
@@ -8,4 +12,21 @@ const s3 = new S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 })
-s3.listBuckets({}).then((data) => console.log(data))
+const file = fs.readFileSync(path.resolve('uploads/images/imgtests3cloud.png'))
+const parallelUploads3 = new Upload({
+  client: s3,
+  params: { Bucket: 'nbb-liveball-ap-southeast-1', Key: 'anh.jpg', Body: file, ContentType: 'image/png' },
+
+  tags: [
+    /*...*/
+  ], // optional tags
+  queueSize: 4, // optional concurrency configuration
+  partSize: 1024 * 1024 * 5, // optional size of each part, in bytes, at least 5MB
+  leavePartsOnError: false, // optional manually handle dropped parts
+});
+
+parallelUploads3.on("httpUploadProgress", (progress) => {
+  console.log(progress);
+});
+
+parallelUploads3.done().then(res => console.log(res))
