f7ade14ebc559c6cc4f074c6070adc16f2ef6c01
Michaelha1808 <haminhchi1808@gmail.com>
2024-02-26 22:01:38 +0700

    Texting back and forth between two people

--- Twitter/src/index.ts
+++ Twitter/src/index.ts
@@ -66,8 +66,11 @@ io.on('connection', (socket) => {
   }
   console.log(users)
   socket.on('private message', (data) => {
-    const receviver_socket_id = users[data.to].socket_id
-    socket.to(receviver_socket_id).emit('receive private message', {
+    const receiver_socket_id = users[data.to]?.socket_id
+    if (!receiver_socket_id) {
+      return
+    }
+    socket.to(receiver_socket_id).emit('receive private message', {
       content: data.content,
       from: user_id
     })
--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,9 +1,32 @@
 import { useEffect, useState } from "react";
 import socket from "./socket";
+import axios from "axios";
 const profile = JSON.parse(localStorage.getItem("profile"));
+const usernames = [
+  {
+    name: "user1",
+    value: "user6596c906348f442190109247",
+  },
+  {
+    name: "user2",
+    value: "user65d847b2cf6270fc066da37f",
+  },
+];
 export default function Chat() {
   const [value, setValue] = useState("");
   const [messages, setMessages] = useState([]);
+
+  const [receiver, setReceiver] = useState("");
+  const getProfile = (username) => {
+    axios
+      .get(`/users/${username}`, {
+        baseURL: import.meta.env.VITE_API_URL,
+      })
+      .then((res) => {
+        setReceiver(res.data.result._id);
+        alert(`Now you can chat with ${res.data.result.name}`);
+      });
+  };
   useEffect(() => {
     socket.auth = {
       _id: profile._id,
@@ -28,7 +51,7 @@ export default function Chat() {
     setValue("");
     socket.emit("private message", {
       content: value,
-      to: "65d847b2cf6270fc066da37f", // user_id
+      to: receiver, // user_id
     });
     setMessages((message) => [
       ...message,
@@ -41,6 +64,15 @@ export default function Chat() {
   return (
     <div>
       <h1>Chat</h1>
+      <div>
+        {usernames.map((username) => (
+          <div key={username.name}>
+            <button onClick={() => getProfile(username.value)}>
+              {username.name}
+            </button>
+          </div>
+        ))}
+      </div>
       <div className="chat">
         {messages.map((message, index) => (
           <div key={index}>
